1e5153f9b38147fd6a00b5efe258655d27863eec
Luis <lvas248@gmail.com>
2022-11-29 15:07:58 -0500

    commit tree 83eec8bbd2c26d9d7472b91a27e935982705ebf5

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :click_bait
+
+
+    private 
+
+    def click_bait
+        unless title.present? && title.include?("Won't Believe" || "Secret" || "Top" || "Guess")
+            errors.add(:title, "This is no click bait!")
+        end
+    end
+
 end
